71db6f37ca5d65bd45a610d615a293348c30bce7
Evgeniy Polyarush <bratikha@gmail.com>
2018-11-14 16:08:51 +1200

    All the source is ready

--- worker/index.js
+++ worker/index.js
@@ -6,3 +6,16 @@ const redisClient = redis.createClient ({
     port: keys.redisPort,
     retry_strategy: () => 1000 // reconnect if connection is lost every ...ms
 });
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message)  => {
+    redisClient.hset('values', message, fib(parseInt(message)));
+}) 
+
+sub.subscribe('insert');
